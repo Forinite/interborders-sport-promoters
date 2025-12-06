4330f9df2009fac503be2da24a3a06e905fbca15
Forinite <obefortune2@gmail.com>
2025-12-07 00:11:19 +0100

    Story display issue handled

--- app/(components)/stories/page.tsx
+++ app/(components)/stories/page.tsx
@@ -55,7 +55,7 @@ export default async function StoriesPage() {
                 </div>
 
                 {/* Rest of the Grid */}
-                <StoryGrid stories={allStories.slice(1)} />
+                <StoryGrid stories={allStories} />
             </div>
         </div>
     );
